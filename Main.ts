26c67adbf87ad1e8f428b0b8a8f1278090d77b2e
alefr19811021 <alefr19811021@gmail.com>
2026-09-03 20:57:07 -0300

    Add files via upload

--- Main.ts
+++ Main.ts
@@ -10,13 +10,67 @@ teclado("Pressione alguma tecla para continuar...")
 const equipamento: Equipamento = new Equipamento("ferramentas", "uteis", 0)
 
 const sherman: Tanque = new Tanque("M4", "Medio", equipamento);
-sherman.vida = 200;
-sherman.dano = 85;
+
+
 
 //console.table(sherman.map(p=>({modelo:p.modelo , classe:p.classe})));
-console.table(sherman);
-console.table(equipamento);
+//console.table(sherman);
+//console.table(equipamento);
 
+
+let escolha
+do {
+    console.log("\n\n\nTa ta-ra -raaaaaaaaaaaaaaaaa!!!!\n\n")
+    console.log("1.carregar canhao")
+    console.log("2.reparar")
+    console.log("3.Status")
+    console.log("4.Atacar")
+    escolha=+teclado("\n\nEscolha uma opção: ")
+
+    
+    switch (escolha) {
+
+    case 1: //carregar canhao
+        try{
+            sherman.carregarMunicao();
+        }catch(e){
+            console.log((e as Error).message);            
+        }
+    break;
+        
+    case 2:  //reparar
+        try{
+            sherman.repararTanque();
+        }catch(e){
+            console.log((e as Error).message);
+        }
+    break;
+
+    case 3:  //status
+        try{
+            console.table(sherman);
+        }catch(e){
+            console.log((e as Error).message);
+        }
+        
+    break;
+
+    case 4: //atacar
+        try{
+            sherman.atacar()
+            console.log("BOOOOOMMMMMMMMM!!!!!!!!!!!!!")
+        }catch(e){
+            console.log((e as Error).message);
+        }
+        
+        
+    default:
+    break;
+
+    }
+
+    
+} while (escolha < 9);
 
 
 
