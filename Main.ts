9ce34fb1f36db5243e778837215dbd6895f7c1ac
alefr19811021 <alefr19811021@gmail.com>
2026-09-03 19:06:33 -0300

    Add files via upload

--- Main.ts
+++ Main.ts
@@ -1,4 +1,5 @@
-import { Tanque } from "./Personagem.ts";
+import { Tanque } from "./class/Personagem.ts";
+import { Equipamento } from "./class/Equipamento.ts";
 import prompt from "prompt-sync"
 
 const teclado = prompt()
@@ -6,11 +7,17 @@ const teclado = prompt()
 teclado("Pressione alguma tecla para continuar...")
 
 
-const p: Tanque = new Tanque();
-p.modelo = "Edécio";
-p.vida = 10;
-p.classe = "Eunuco";
-p.dano = "Livro de lógica - Bem legal";
-p.equipamentos = 
+const equipamento: Equipamento = new Equipamento("ferramentas", "uteis", 0)
 
-console.table(p);
+const sherman: Tanque = new Tanque("M4", "Medio", equipamento);
+sherman.vida = 200;
+sherman.dano = 85;
+
+//console.table(sherman.map(p=>({modelo:p.modelo , classe:p.classe})));
+console.table(sherman);
+console.table(equipamento);
+
+
+
+
+//Escolha nos Modelos de veículos; Classe; Equipamentos; Dano; Vida;
